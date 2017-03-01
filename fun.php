9d24b8d1868e2ec7d19e8798c9bb8667dda05577
xingyue <qixingyue@gmail.com>
2017-03-01 09:48:06 +0800

    Update fun.php

--- fun.php
+++ fun.php
@@ -7,8 +7,6 @@ function real_test($idnum,$name){
 
 	include './vendor/autoload.php';
 
-	//$idnum = '130123198706234534';
-	//$name = '祁星月';
 	$url = 'http://www.runchina.org.cn/portal.php?mod=score&ac=personal';
 
 	$headstr = "-H 'Pragma: no-cache' -H 'Origin: http://www.runchina.org.cn' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4,ja;q=0.2' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cache-Control: no-cache' -H 'Referer: http://www.runchina.org.cn/portal.php?mod=score&ac=personal' -H 'Cookie: SMHa_2132_saltkey=tQi19Tmq; SMHa_2132_lastvisit=1468828350; SMHa_2132_sid=l9z09Q; SMHa_2132_lastact=1468832009%09portal.php%09score; Hm_lvt_b0a502205e02e6127ae831a751ff05b3=1468831949; Hm_lpvt_b0a502205e02e6127ae831a751ff05b3=1468832009' -H 'Connection: keep-alive'";
@@ -44,4 +42,4 @@ function real_test($idnum,$name){
 	echo json_encode($real_items);
 }
 
-//real_test('130123198706234534','祁星月');
+//real_test('idnum','xxx');
